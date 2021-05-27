9ab6ed72f041bfce438360e46e4e8447bf933d93
akshikamudgal <66902249+akshikamudgal@users.noreply.github.com>
2021-05-27 11:56:57 +0530

    Add files via upload

---
+++ bubble.java
@@ -0,0 +1,36 @@
+package com.example.lib3;
+
+public class bubble {
+        void bubbleSort(int arr[])
+        {
+            int n = arr.length;
+            for (int i = 0; i < n-1; i++)
+                for (int j = 0; j < n-i-1; j++)
+                    if (arr[j] > arr[j+1])
+                    {
+                        // swap arr[j+1] and arr[j]
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+        }
+
+        /* Prints the array */
+        void printArray(int arr[])
+        {
+            int n = arr.length;
+            for (int i=0; i<n; ++i)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+
+        // Driver method to test above
+        public static void main(String args[])
+        {
+            bubble ob = new bubble();
+            int arr[] = {64, 34, 25, 12, 22, 11, 90};
+            ob.bubbleSort(arr);
+            System.out.println("Sorted array");
+            ob.printArray(arr);
+        }
+}
